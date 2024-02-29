09da6d0d2e51f86afb0e7be1ef8bbc3cee199ee2
Shyak Das <shayak.das007@gmail.com>
2024-02-29 21:11:55 +0530

    [Implementation]Home Screen Implementation Done

--- app/src/main/java/com/news/app/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/news/app/presentation/home/HomeScreen.kt
@@ -1,25 +1,35 @@
 package com.news.app.presentation.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.paging.compose.LazyPagingItems
 import com.news.app.R
 import com.news.app.domain.model.Article
 import com.news.app.presentation.Dimens
+import com.news.app.presentation.common.ArticleList
+import com.news.app.presentation.common.SearchBar
+import com.news.app.presentation.navgraph.Route
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(articles: LazyPagingItems<Article>, navigate: (String) -> Unit) {
     val titles by remember {
@@ -48,5 +58,31 @@ fun HomeScreen(articles: LazyPagingItems<Article>, navigate: (String) -> Unit) {
                 .padding(horizontal = Dimens.MediumPadding1)
         )
         Spacer(modifier = Modifier.height(Dimens.MediumPadding1))
+        SearchBar(
+            modifier = Modifier.padding(horizontal = Dimens.MediumPadding1),
+            text = "",
+            readOnly = true,
+            onValueChange = {},
+            onClick = {
+                navigate(Route.SearchScreen.route)
+            },
+            onSearch = {})
+        Spacer(modifier = Modifier.height(Dimens.MediumPadding1))
+        Text(
+            text = titles,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = Dimens.MediumPadding1)
+                .basicMarquee(),
+            fontSize = 12.sp,
+            color = colorResource(id = R.color.placeholder)
+        )
+        Spacer(modifier = Modifier.height(Dimens.MediumPadding1))
+        ArticleList(
+            modifier = Modifier.padding(horizontal = Dimens.MediumPadding1),
+            articles = articles,
+            onClick = {
+                navigate(Route.DetailsScreen.route)
+            })
     }
 }
