7a347f8fd89559c04ee3891aa3bfdf7d005c6a9f
Shyak Das <shayak.das007@gmail.com>
2024-03-24 01:51:50 +0530

    [Implementation]BottomNavigation design

---
+++ app/src/main/java/com/news/app/presentation/news_navigator/BottomNavigationItem.kt
@@ -0,0 +1,5 @@
+package com.news.app.presentation.news_navigator
+
+import androidx.annotation.DrawableRes
+
+data class BottomNavigationItem(@DrawableRes val icon : Int, val text : String)
---
+++ app/src/main/java/com/news/app/presentation/news_navigator/components/NewsBottomNavigation.kt
@@ -0,0 +1,82 @@
+package com.news.app.presentation.news_navigator.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.news.app.R
+import com.news.app.presentation.Dimens
+import com.news.app.presentation.news_navigator.BottomNavigationItem
+import com.news.app.ui.theme.NewsAppTheme
+
+@Composable
+fun NewsBottomNavigation(
+    items: List<BottomNavigationItem>,
+    selected: Int,
+    onItemClick: (Int) -> Unit
+) {
+    NavigationBar(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.background,
+        tonalElevation = 10.dp
+    ) {
+        items.forEachIndexed { index, items ->
+            NavigationBarItem(
+                selected = index == selected,
+                onClick = { onItemClick(index) },
+                icon = {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(
+                            painter = painterResource(id = items.icon),
+                            contentDescription = null,
+                            modifier = Modifier.size(Dimens.IconSize)
+                        )
+
+                        Spacer(modifier = Modifier.height(Dimens.ExtraSmallPadding2))
+
+                        Text(text = items.text, style = MaterialTheme.typography.labelSmall)
+                    }
+                }, colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = colorResource(id = R.color.body),
+                    unselectedTextColor = colorResource(id = R.color.body)
+                )
+            )
+        }
+    }
+}
+
+
+@Composable
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun NewsBottomNavigationPreview() {
+    NewsAppTheme {
+        NewsBottomNavigation(
+            items = listOf(
+                BottomNavigationItem(
+                    icon = R.drawable.ic_home,
+                    text = "Home"
+                ),
+                BottomNavigationItem(icon = R.drawable.ic_search, text = "Search"),
+                BottomNavigationItem(icon = R.drawable.ic_bookmark, text = "Bookmark")
+            ), selected = 0, onItemClick = {}
+        )
+    }
+}
